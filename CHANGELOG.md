90db39dbe089ceb08895063164c6f025835e36ae
Daniel Solarte <danielsolartech@hotmail.com>
2021-02-03 19:43:59 -0500

    feat: Add gradient, indigo and deep_purple nextchat widget types

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## Unreleased
 
 ## 03/02/2021
+-   Add `gradient`, `indigo` and `deep_purple` NextChat widget types.
 -   Add `login` page.
 -   Add `chat_button`, `chat_input` and `nextchat` widgets.
 -   Add `gradient_text` dependency.
--- lib/widgets/nextchat.dart
+++ lib/widgets/nextchat.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:gradient_text/gradient_text.dart';
 
+enum NextChatType { gradient, indigo, deep_purple }
+
 class NextChat extends StatelessWidget {
-  NextChat({Key key, this.size = 16.0, this.margin = const EdgeInsets.all(0.0)})
+  NextChat(
+      {Key key,
+      this.type = NextChatType.gradient,
+      this.size = 16.0,
+      this.margin = const EdgeInsets.all(0.0)})
       : super(key: key);
 
+  final NextChatType type;
   final double size;
   final EdgeInsetsGeometry margin;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: this.margin,
-        child: Center(
-            child: GradientText('NextChat',
-                gradient: LinearGradient(
-                    colors: [Colors.deepPurple, Colors.indigo],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter),
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: this.size,
-                ))));
+    Widget child;
+
+    if (this.type == NextChatType.gradient) {
+      child = GradientText('NextChat',
+          gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.indigo],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter),
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: this.size,
+          ));
+    } else {
+      Color color;
+
+      if (this.type == NextChatType.deep_purple) {
+        color = Colors.deepPurple;
+      } else {
+        color = Colors.indigo;
+      }
+
+      child = Text('NextChat',
+          style: TextStyle(
+              color: color, fontSize: this.size, fontWeight: FontWeight.w500));
+    }
+
+    return Container(margin: this.margin, child: Center(child: child));
   }
 }
