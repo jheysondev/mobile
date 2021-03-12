796d10301f87f2f962d96f675d160af80f57e423
Daniel Solarte <danielsolartech@hotmail.com>
2021-03-11 20:08:07 -0500

    feat: Update routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,14 +1,25 @@
 
 import 'package:flutter/material.dart';
+import 'package:nextchat/screens/home/home_screen.dart';
+import 'package:nextchat/screens/messages/messages_screen.dart';
+import 'package:nextchat/screens/notifications/notifications_screen.dart';
+import 'package:nextchat/screens/profile/profile_screen.dart';
 import 'package:nextchat/screens/signInOrSignUp/signin_screen.dart';
 import 'package:nextchat/screens/signInOrSignUp/signup_screen.dart';
 import 'package:nextchat/screens/signInOrSignUp/welcome_screen.dart';
 
 class Routes extends StatelessWidget {
   final routes = <String, WidgetBuilder>{
+    // SignInOrSignUp
     "/welcome": (BuildContext context) => WelcomeScreen(),
     "/signin": (BuildContext context) => SignInScreen(),
     "/signup": (BuildContext context) => SignUpScreen(),
+
+    // Logged
+    "/home": (BuildContext context) => HomeScreen(),
+    "/messages": (BuildContext context) => MessagesScreen(),
+    "/notifications": (BuildContext context) => NotificationsScreen(),
+    "/profile": (BuildContext context) => ProfileScreen(),
   };
 
   @override
@@ -18,6 +29,7 @@ class Routes extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
+      debugShowCheckedModeBanner: false,
       initialRoute: "/welcome",
       routes: routes,
     );
